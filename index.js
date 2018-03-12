c238c41e5c58d8e4ee5a435739da81d976eb8b94
Cyril Wanner <cyril.wanner@hotmail.com>
2018-03-12 21:36:20 +0100

    Optimize images

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-module.exports = ({ inlineImageLimit, imagesFolder, ...nextConfig } = {}) => {
+/**
+ * Converts possible configuration values for an image optimizer.
+ *
+ * @param {object|boolean|null} config - configuration for the optimizer
+ * @param {object} defaultOptions - default configuration if `config` is undefined (optional)
+ * @returns {object|boolean} configuration object or false if this optimizer is disabled
+ */
+const getOptimizerConfig = (config, defaultOptions = {}) => {
+  if (config === null || config === false) {
+    return false;
+  }
+
+  return config || defaultOptions;
+};
+
+/**
+ * Build the regex for webpack for all image types which should get handled by this plugin.
+ *
+ * @param {object|boolean|null} mozjpeg - configuration for mozjpeg or false to disable handling jpg files
+ * @param {object|boolean|null} optipng - configuration for optipng or false to disable handling png files if pngquant is also set to false
+ * @param {object|boolean|null} pngquant - configuration for pngquant or false to disable handling png files if optipng is also set to false
+ * @param {object|boolean|null} gifsicle - configuration for gifsicle or false to disable handling gif files
+ * @param {object|boolean|null} svgo - configuration for svgo or false to disable handling svg files
+ * @returns {RegExp} RegExp matching all files which this plugin should handle
+ */
+const getHandledFilesRegex = (mozjpeg, optipng, pngquant, gifsicle, svgo) => {
+  const handledFiles = [];
+
+  if (mozjpeg !== false) {
+    handledFiles.push('jpe?g');
+  }
+  if (optipng !== false || pngquant !== false) {
+    handledFiles.push('png');
+  }
+  if (gifsicle !== false) {
+    handledFiles.push('gif');
+  }
+  if (svgo !== false) {
+    handledFiles.push('svg');
+  }
+
+  return new RegExp(`\.(${handledFiles.join('|')})$`, 'i');
+};
+
+/**
+ * Configure webpack and next.js to handle and optimize images with this plugin.
+ *
+ * @param {object} param0 - configuration for next-optimized-plugins, see the readme for possible values
+ */
+const withOptimizedImages = ({
+  inlineImageLimit = 8192,
+  imagesFolder = 'images',
+  imagesName = '[name]-[hash].[ext]',
+  mozjpeg,
+  optipng,
+  pngquant = false,
+  gifsicle,
+  svgo,
+  ...nextConfig
+} = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
       if (!options.defaultLoaders) {
@@ -7,18 +66,38 @@ module.exports = ({ inlineImageLimit, imagesFolder, ...nextConfig } = {}) => {
         );
       }
 
+      // build options for url-loader with file-loader as fallback
+      const urlLoaderOptions = {
+        limit: inlineImageLimit,
+        fallback: 'file-loader',
+        publicPath: `/_next/static/${imagesFolder}/`,
+        outputPath: `static/${imagesFolder}/`,
+        name: imagesName,
+      };
+
+      // build options for img-loader
+      const imgLoaderOptions = {
+        mozjpeg: getOptimizerConfig(mozjpeg),
+        optipng: getOptimizerConfig(optipng),
+        pngquant: getOptimizerConfig(pngquant),
+        gifsicle: getOptimizerConfig(gifsicle, {
+          interlaced: true,
+          optimizationLevel: 3,
+        }),
+        svgo: getOptimizerConfig(svgo),
+      };
+
+      // push the loaders to the webpack configuration of next.js
       config.module.rules.push({
-        test: /\.(jpe?g|png|gif|svg)$/i,
+        test: getHandledFilesRegex(mozjpeg, optipng, pngquant, gifsicle, svgo),
         use: [
           {
             loader: 'url-loader',
-            options: {
-              limit: inlineImageLimit || 8192,
-              fallback: 'file-loader',
-              publicPath: `/_next/static/${imagesFolder || 'images'}/`,
-              outputPath: `static/${imagesFolder || 'images'}/`,
-              name: '[name]-[hash].[ext]',
-            },
+            options: urlLoaderOptions,
+          },
+          {
+            loader: 'img-loader',
+            options: imgLoaderOptions,
           },
         ],
       });
@@ -31,3 +110,5 @@ module.exports = ({ inlineImageLimit, imagesFolder, ...nextConfig } = {}) => {
     }
   })
 }
+
+module.exports = withOptimizedImages;
