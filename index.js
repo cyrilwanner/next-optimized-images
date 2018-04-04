157cf4c314221b452356af53824a70133aa531fb
Cyril Wanner <cyril.wanner@hotmail.com>
2018-04-04 11:53:13 +0200

    Optimize svg icons in sprite

--- index.js
+++ index.js
@@ -160,16 +160,6 @@ const withOptimizedImages = (nextConfig) => {
       config.module.rules.push({
         test: getHandledFilesRegex(mozjpeg, optipng, pngquant, gifsicle, svgo),
         oneOf: [
-          // ?sprite: add icon to sprite
-          {
-            resourceQuery: /sprite/,
-            use: [
-              {
-                loader: 'svg-sprite-loader',
-                options: getOptimizerConfig(svgSpriteLoader),
-              },
-            ],
-          },
           // ?include: include the image directly, no data uri or external file
           // ?inline: force inlining an image regardless of the defined limit
           ...getResourceQueryLoaders('img-loader', imgLoaderOptions, urlLoaderOptions),
@@ -194,6 +184,22 @@ const withOptimizedImages = (nextConfig) => {
                 options: webpLoaderOptions || {},
               },
             ],
+          },
+
+          // ?sprite: add icon to sprite
+          {
+            resourceQuery: /sprite/,
+            use: [
+              {
+                loader: 'svg-sprite-loader',
+                options: getOptimizerConfig(svgSpriteLoader),
+              },
+            ].concat(imgLoaderOptions.svgo !== false ? [
+              {
+                loader: 'img-loader',
+                options: imgLoaderOptions,
+              }
+            ] : []),
           },
 
           // default behavior: inline if below the definied limit, external file if above
