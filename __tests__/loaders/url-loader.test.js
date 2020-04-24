ec515eef75594c8e7e4bcc55e53fc1fc27e7053c
Cyril Wanner <info@cyr.li>
2020-04-24 18:00:58 +0200

    Ensure the correct file-loader version gets used

--- __tests__/loaders/url-loader.test.js
+++ __tests__/loaders/url-loader.test.js
@@ -1,12 +1,13 @@
 const { getConfig } = require('../../lib/config');
 const { getUrlLoaderOptions } = require('../../lib/loaders/url-loader');
+const { getFileLoaderPath } = require('../../lib/loaders/file-loader');
 
 describe('next-optimized-images/loaders/url-loader', () => {
   it('uses the default config', () => {
     const options = getUrlLoaderOptions(getConfig({}), false);
 
     expect(options.limit).toEqual(8192);
-    expect(options.fallback).toEqual('file-loader');
+    expect(options.fallback).toEqual(getFileLoaderPath());
     expect(options.name).toEqual('[name]-[hash].[ext]');
   });
 
